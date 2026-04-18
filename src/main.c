c1f7681b4bad2dba38ad19eb3f1542256dc39ba8
RIley <56058026+indiebear530@users.noreply.github.com>
2026-04-18 05:32:39 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,31 +1,141 @@
-cmake_minimum_required(VERSION 3.5)
+#include <psp2/ctrl.h>
+#include <psp2/display.h>
+#include <psp2/ime_dialog.h>
+#include <psp2/touch.h>
+#include <psp2/kernel/processmgr.h>
+#include <psp2/net/http.h>
+#include <psp2/net/net.h>
+#include <psp2/net/netctl.h>
 
-project(vita_browser)
+#include <vita2d.h>
+#include <string.h>
+#include <stdlib.h>
 
-include("$ENV{VITASDK}/share/vita.cmake" REQUIRED)
+static char url[256] = "http://example.com";
+static char page[200000];
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wl,-q -Wall -O3")
+static int scroll_y = 0;
+static int dragging = 0;
+static int last_y = 0;
 
-add_executable(vita_browser
-    src/main.c
-)
+/* -------- HTTP -------- */
+char* http_get(const char *u) {
+    memset(page, 0, sizeof(page));
 
-target_link_libraries(vita_browser
-    vita2d
-    SceCtrl_stub
-    SceDisplay_stub
-    SceGxm_stub
-    SceSysmodule_stub
-    SceTouch_stub
-    SceHttp_stub
-    SceNet_stub
-    SceNetCtl_stub
-    SceKernel_stub
-)
+    SceUID tpl = sceHttpCreateTemplate("vita", 1, 1);
+    SceUID conn = sceHttpCreateConnectionWithURL(tpl, u, 0);
+    SceUID req  = sceHttpCreateRequestWithURL(conn, SCE_HTTP_METHOD_GET, u, 0);
 
-vita_create_self(eboot.bin vita_browser UNSAFE)
+    sceHttpSendRequest(req, NULL, 0);
+    sceHttpReadData(req, page, sizeof(page)-1);
 
-vita_create_vpk(vita_browser.vpk "VITABRWS" eboot.bin
-    VERSION "01.00"
-    NAME "Vita Browser"
-)
+    sceHttpDeleteRequest(req);
+    sceHttpDeleteConnection(conn);
+    sceHttpDeleteTemplate(tpl);
+
+    return page;
+}
+
+/* -------- Keyboard -------- */
+void open_keyboard() {
+    SceImeDialogParam param;
+    sceImeDialogParamInit(&param);
+
+    param.type = SCE_IME_TYPE_URL;
+    param.title = "Enter URL";
+    param.maxTextLength = 256;
+
+    strcpy(param.inputTextBuffer, url);
+
+    sceImeDialogInit(&param);
+
+    while (sceImeDialogGetStatus() != SCE_COMMON_DIALOG_STATUS_FINISHED) {
+        sceKernelDelayThread(10000);
+    }
+
+    SceImeDialogResult result;
+    sceImeDialogGetResult(&result);
+
+    if (result.button == SCE_IME_DIALOG_BUTTON_ENTER) {
+        strcpy(url, param.inputTextBuffer);
+        http_get(url);
+    }
+
+    sceImeDialogTerm();
+}
+
+/* -------- Touch -------- */
+void handle_touch() {
+    SceTouchData touch;
+    sceTouchPeek(SCE_TOUCH_PORT_FRONT, &touch, 1);
+
+    if (touch.reportNum > 0) {
+        int y = touch.report[0].y / 2;
+
+        if (y < 60) {
+            open_keyboard();
+        } else {
+            if (!dragging) {
+                dragging = 1;
+                last_y = y;
+            } else {
+                scroll_y += (y - last_y);
+                last_y = y;
+            }
+        }
+    } else {
+        dragging = 0;
+    }
+}
+
+/* -------- MAIN -------- */
+int main() {
+    sceCtrlSetSamplingMode(SCE_CTRL_MODE_ANALOG);
+    sceTouchSetSamplingState(SCE_TOUCH_PORT_FRONT, SCE_TOUCH_SAMPLING_STATE_START);
+
+    vita2d_init();
+    http_get(url);
+
+    while (1) {
+        SceCtrlData pad;
+        sceCtrlPeekBufferPositive(0, &pad, 1);
+
+        if (pad.buttons & SCE_CTRL_START)
+            break;
+
+        if (pad.buttons & SCE_CTRL_TRIANGLE)
+            open_keyboard();
+
+        if (pad.buttons & SCE_CTRL_CIRCLE)
+            http_get(url);
+
+        handle_touch();
+
+        vita2d_start_drawing();
+        vita2d_clear_screen();
+
+        /* URL BAR */
+        vita2d_draw_rectangle(0, 0, 960, 60, RGBA8(20,20,20,255));
+        vita2d_draw_string(10, 20, RGBA8(255,255,0,255), url);
+
+        /* PAGE */
+        int y = 80 + scroll_y;
+
+        for (int i = 0; page[i]; i++) {
+            if (page[i] == '<') {
+                while (page[i] && page[i] != '>') i++;
+            } else {
+                vita2d_draw_char(10, y, RGBA8(255,255,255,255), page[i]);
+                y += 10;
+            }
+        }
+
+        vita2d_end_drawing();
+        vita2d_swap_buffers();
+        sceDisplayWaitVblankStart();
+    }
+
+    vita2d_fini();
+    sceKernelExitProcess(0);
+    return 0;
+}
