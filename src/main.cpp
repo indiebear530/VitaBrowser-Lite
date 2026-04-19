ac0aebcba78632594686532ff1aba9dc77c2153b
RIley B <56058026+indiebear530@users.noreply.github.com>
2026-04-19 01:05:47 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,10 @@
+#include <vitasdk.h>
 #include <vita2d.h>
-#include <psp2/ctrl.h>
-#include <psp2/display.h>      // ← this is the missing one for sceDisplayWaitVblankStart
-#include <psp2/kernel/processmgr.h>
+#include <stdio.h>
 
-vita2d_pgf *pgf;
-
-int main()
-{
+int main(int argc, char *argv[]) {
     vita2d_init();
-    pgf = vita2d_load_default_pgf();
+    vita2d_pgf *pgf = vita2d_load_default_pgf();
 
     SceCtrlData pad;
 
@@ -24,7 +20,6 @@ int main()
 
         vita2d_pgf_draw_text(pgf, 120, 120, RGBA8(255, 255, 255, 255), 1.2f, "VitaBrowser Lite");
         vita2d_pgf_draw_text(pgf, 120, 180, RGBA8(180, 220, 255, 255), 0.9f, "Press START to exit");
-
         vita2d_pgf_draw_text(pgf, 120, 280, RGBA8(100, 255, 100, 255), 0.8f, "Basic graphics test running...");
 
         vita2d_end_drawing();
@@ -34,6 +29,5 @@ int main()
 
     vita2d_free_pgf(pgf);
     vita2d_fini();
-    sceKernelExitProcess(0);
     return 0;
 }
